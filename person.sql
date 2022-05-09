c90698406beb4e8a04896cf396818fcfef97f7f0
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-09 15:08:48 -0600

    part 2 poblems 2-6 done

--- person.sql
+++ person.sql
@@ -7,3 +7,27 @@ CREATE TABLE person(
   	city VARCHAR(50),
   	height_cm INT
   );
+
+2.
+INSERT INTO person(name, age, height_cm, city, favorite_color)
+	VALUES('Ethan', 22, 178, 'South Jordan', 'Black'),
+    ('Calvin', 50, 190, 'Long Beach', 'Blue'),
+    ('Kendrick', 34, 165, 'Compton', 'Red'),
+    ('Malcom', 26, 187, 'Pittsburgh', 'Yellow'),
+    ('Joey', 27, 178, 'Brooklyn', 'Green');
+
+3.
+select name, height_cm from person
+order by height_cm desc;
+
+4.
+select name, height_cm from person
+order by height_cm asc;
+
+5.
+select name, age from person
+order by age desc;
+
+6.
+select name, age from person
+order by age > 20;
