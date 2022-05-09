463f26613ffc494f731a139aee27171e33a03329
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-09 15:59:27 -0600

    part 3 problems 1-4 solved

---
+++ orders.sql
@@ -0,0 +1,23 @@
+1.
+ CREATE TABLE orders (
+   	order_id SERIAL PRIMARY KEY,
+   	person_id INT,
+   	product_name VARCHAR(30),
+   	product_price FLOAT,
+   	quantity INT
+   );
+
+2.
+INSERT INTO orders(person_id, product_name, product_price, quantity)
+ 	VALUES(1, 'Clock', 15.99, 1),
+    (1, 'Hat', 23.99, 1),
+    (1, 'Chair', 42.59, 3),
+    (2, 'Clock', 15.99, 1),
+    (2, 'Hat', 23.99, 2);
+
+3.
+select * from orders
+
+4.
+select sum(quantity)
+from orders;
